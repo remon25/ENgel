3937c8724547a18981107c010b0308374653168d
remon25 <70450564+remon25@users.noreply.github.com>
2025-01-19 03:37:30 +0200

    edit

--- src/app/_components/layout/SearchBar.js
+++ src/app/_components/layout/SearchBar.js
@@ -1,6 +1,6 @@
 export default function SearchBar({ value, onChange }) {
   return (
-    <div className="pt-2 px-4 pb-6 relative mx-auto text-gray-600">
+    <div className="w-full pt-2 px-4 pb-6 relative mx-auto text-gray-600">
       <input
         className="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
         type="search"
